234a38c6d19d31918d4872f4c192948ed1e07684
nikolaskozlan <maxsuccess4@gmail.com>
2024-09-19 21:21:00 +0300

    Adding 2-nd lesson

---
+++ lesson_2.py
@@ -0,0 +1,139 @@
+
+# DataTypes
+
+
+
+# Anmutable / не змінні
+
+# Numeric
+
+# int() 10
+# float() 10.5       0.00001
+# decimal 10.5       0.00001 більш точний
+# complex()          (1 + 2j)
+
+# str рядок
+
+# bool Логічний оператор(True/False)
+# None
+
+# tuple() кортеж
+
+
+
+
+# Mutable / Змінні
+
+# list() список/масив
+# dict() словник
+# set() множина
+
+
+
+
+
+
+# box_number_1 = "apple"  #snake case
+#
+# print("hello")
+#
+# my_print = 333
+# proj_globals = []
+
+
+# num_1 = 5
+#
+# print(num_1)
+#
+#
+# num_1 = 3
+#
+# print(num_1)
+
+# some_list = []
+#
+# some_list_2 = some_list
+#
+# some_list = [1, 2, 3]
+#
+# print(some_list_2)
+
+
+# num_1 = 5.7
+#
+# print(num_1, id(num_1), type(num_1))
+
+# num_1 = 3
+#
+# print(num_1, id(num_1), type(num_1))
+
+
+# num_1 = 11
+# num_2 = 2.5
+
+# result_add = num_1 + num_2
+
+# num_1 += num_2
+# num_1 = num_1 + num_2
+
+
+#
+# print(result_add)
+# result_sub = num_1 - num_2
+# result_div = num_1 / num_2
+# result_div_2 = num_1 // num_2   # цілочислене ділення
+# result_div_3 = num_1 % num_2    # залишок від ділення
+#
+# result_div_4 = divmod(num_1, num_2)    # містить в собі резщульат цілочисленого ділення та залишку від ділення
+#
+# result_mul = num_1 * num_2
+# result_power = num_1 ** num_2   # зведення в ступінь
+# result_root = num_1 ** 0.5      # зведення в ступінь
+#
+#
+# print(result_div_4, type(result_div_4))
+
+# val_str = "hello"
+# val_str = 'hello'
+# val_str = """hello"""
+# val_str = '''hello'''
+
+# val_str = """hello I'm Nick "Kyiv" """
+# print(val_str)
+
+# val_str = "hello I'm"
+# # name = "Nick"
+# name = 1
+#
+# greetings = val_str + " " + str(name)
+# # greetings = f"hello I'm {name}"
+#
+# print(val_str, name)
+
+
+
+############# Constants ################
+
+# some_value = "!"
+#
+# NUMBER_OF_PI = 3.14
+# MONTHS_IN_YEAR = 12
+
+
+############## input() ############### ALWAYS STRING
+
+# number_1 = input("Type some number: ")
+# number_2 = input("Type some number: ")
+#
+# result = int(number_1) + int(number_2)
+#
+# print(result)
+
+
+
+
+
+
+
+
+
