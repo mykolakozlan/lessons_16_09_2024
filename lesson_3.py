1c380ba13ac0c55173039cbce01aae37a3a64733
nikolaskozlan <maxsuccess4@gmail.com>
2024-09-23 19:16:02 +0300

    Adding 3-nd lesson

---
+++ lesson_3.py
@@ -0,0 +1,68 @@
+# Bool, None types
+# Logic operators
+# Зведення типів
+# Duck typing
+# If statement (programming golf, Тернарний оператор)
+# list
+# змінні не змінні типи даних різниця
+# методи list(append(), pop(), sort(), copy())
+# функція sorted()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ############################################
+#
+# base_list = [1, 2, 3]
+# my_new_list = base_list * 4
+#
+#
+# base_list[0] = 10
+#
+# print(f"base_list {base_list}")
+# print(f"my_new_list {my_new_list}")
+#
+# # # ######################################
+#
+# base_list = [1, 2, 3]
+#
+# new_list = [base_list] * 4
+# print(new_list)
+#
+# base_list[0] = 10
+# print(f"base_list {base_list}")
+# print(new_list)
+
+# # # ######################################
