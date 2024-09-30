e56fa3235fec5692cbbfe5e256a4fc1c0ddc3b1c
nikolaskozlan <maxsuccess4@gmail.com>
2024-09-30 20:32:32 +0300

    Adding 4-th lesson

---
+++ lesson_5.py
@@ -0,0 +1,176 @@
+
+
+# first_symbol = float(input("1st num:  "))
+# my_operator = input("operator (+, -, /, *):  ")
+# second_symbol = float(input("2nd num:  "))
+#
+# result = None
+#
+# # if operator != "+" or operator != "-" or operator != "/" or operator != "*":
+# # if operator not in "+-*/" or len(operator) != 1:
+# # if operator not in ["+", "-", "*", "/"]:
+# if my_operator not in ("+", "-", "*", "/"):
+#     result = "ERROR - wrong operator"
+#
+#
+# if my_operator == "+":
+#     result = first_symbol + second_symbol
+# elif my_operator == "-":
+#     result = first_symbol - second_symbol
+# elif my_operator == "*":
+#     result = first_symbol * second_symbol
+# elif my_operator == "/":
+#     if second_symbol != 0:
+#         result = first_symbol / second_symbol
+#     else:
+#         result = "ERROR - can't divide by zero"
+# # else:
+# #     result = "ERROR - wrong operator"
+#
+#
+# print(result)
+
+
+
+
+
+
+
+
+# original_list =  [12]
+#
+# # last_element = original_list[-1]
+# # original_list.pop()
+# #
+# # original_list.insert(0, last_element)
+#
+# # if original_list:
+# if len(original_list) > 1:
+#     original_list.insert(0, original_list.pop())
+#
+# print(original_list)
+
+
+
+
+
+# my_list = [1, 2, 3, 9, 4, 5, 6]
+#
+# separator = len(my_list) - len(my_list) // 2
+# new_list = [my_list[:separator], my_list[separator:]]
+#
+# print(new_list)
+
+
+
+############################ String #########################
+
+# value_str = "hello"
+# print(value_str, id(value_str))
+# value_str = "hello!"
+# print(value_str, id(value_str))
+
+# print(value_str[0])
+# print(value_str[1:4])
+# print(value_str[14])
+# print(value_str[14:18], type(value_str[14:18]))
+# print(value_str[::2])
+# print(value_str[::-1])
+
+# value_str = "email@gmail.com"
+
+# "Email@gmail.com"
+
+# first_name = "Nick Dev"
+
+# is_confirmed = True
+
+
+# value_lower_case = value_str.lower()
+# value_upper_case = value_str.upper()
+# value_capital = first_name.capitalize()
+# value_title = first_name.title()
+
+# value_swapcase = first_name.swapcase()
+
+
+# print(first_name)
+# print(value_swapcase)
+
+
+# value_str = "Nick Dev"
+# # print(value_str.isalpha())
+# print(value_str.isspace())
+
+
+# if not value_str.isalpha():
+#     value_float = float(value_str)
+#     print(value_float)
+#
+#
+# print("end")
+#
+# value_str = "hello"
+#
+# result = ""
+#
+# # for letter in value_str:
+# #     if letter.isdigit():
+# #         result += letter
+#
+# # print(result)
+#
+# # enumerate()
+#
+# for index, letter in enumerate(value_str):
+#     print(index, letter)
+
+value_str = "hellool"
+
+# some_method = value_str.find("loo")
+# some_method_2 = value_str.rfind("loo")
+# print(some_method)
+# print(some_method_2)
+
+# if value_str.startswith("h"):
+#     print(True)
+# value_str.endswith()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
